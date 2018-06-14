48ffdb6ce9f85d638abc82584cb2d9d6746bec45
casa <casa@casas-iMac.lan>
2018-06-14 10:05:15 -0300

    add category session

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,32 +1,71 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
+import withRoot from '../withRoot';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 class App extends Component {
   render() {
+    //const { categories } = this.props;
+    const { match } = this.props;
+    console.log(this.props);
+    const categories = ['react', 'redux', 'udacity'];
     return (
       <div>
-        <Switch>
-          <Route exact path="/" render={() => <div>Lista posts</div>} />
-          <Route path="/posts/new" render={() => <div>Adiciona post</div>} />
-          <Route path="/posts/:id/edit" render={() => <div>Edita post</div>} />
-          <Route path="/posts/:id" render={() => <div>Detalha post</div>} />
-          <Route
-            path="/comment/:id/edit"
-            render={() => <div>Edita post</div>}
-          />
-          <Route
-            path="/comment/:id"
-            render={() => <div>Edita comentário</div>}
-          />
-          <Route
-            path="/:category"
-            render={() => <div>Lista posts de categoria</div>}
-          />
-          <Route render={() => <div>Miss</div>} />
-        </Switch>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Readable
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <br />
+          <Paper elevation={4}>
+            <Typography variant="headline" component="h3">
+              Categories:
+            </Typography>
+            <Typography component="p">
+              {categories.map(c => (
+                <span key={c}>
+                  <Link to={'/' + c}>{c}</Link>{' '}
+                </span>
+              ))}
+            </Typography>
+          </Paper>
+
+          <Switch>
+            <Route exact path="/" render={() => <div>Lista posts</div>} />
+            <Route path="/posts/new" render={() => <div>Adiciona post</div>} />
+            <Route
+              path="/posts/:id/edit"
+              render={() => <div>Edita post</div>}
+            />
+            <Route path="/posts/:id" render={() => <div>Detalha post</div>} />
+            <Route
+              path="/comment/:id/edit"
+              render={() => <div>Edita post</div>}
+            />
+            <Route
+              path="/comment/:id"
+              render={() => <div>Edita comentário</div>}
+            />
+            <Route
+              path="/:category"
+              render={() => <div>Lista posts de categoria </div>}
+            />
+            <Route render={() => <div>Miss</div>} />
+          </Switch>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(withRoot(App));
